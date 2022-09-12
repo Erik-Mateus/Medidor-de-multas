2a1de0d6d651496ebdcb41165640f791558faff1
Erik Mateus <rdrderikseixas@gmail.com>
2022-09-12 18:42:40 -0300

    modo escuro agr de verdade

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-
 function calcular() {
   var txtvelo = document.getElementById("txtvel");
   var res = document.getElementById("res");
   var vel = Number(txtvelo.value);
   var multas = txtvelo.value - 50;
   var multasPorCada = multas / 10;
-  var arredondada = Math.floor(multasPorCada, 1)
+  var arredondada = Math.floor(multasPorCada, 1);
 
   res.innerHTML = `sua velocidade é de ${vel}`;
   if (vel > 60) {
     res.innerHTML = `voce ultrapassou o limite de velocidade e ganhou ${arredondada} multas`;
   }
 }
+
+const $html = document.querySelector("html");
+const $checkbox = document.querySelector(".checkbox");
+
+$checkbox.addEventListener('change', function(){
+  $html.classList.toggle('darkmode')
+})
